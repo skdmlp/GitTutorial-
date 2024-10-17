18add9d506c5e47e4386e9251abe25f6159914d6
gadisom <jeongwon.kim@gmail.com>
2024-10-17 16:11:45 +0900

    feat: initial commit

---
+++ Gittuorials/Gittuorials/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gittuorials
+//
+//  Created by 김정원 on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gittuorials/Gittuorials/GittuorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittuorialsApp.swift
+//  Gittuorials
+//
+//  Created by 김정원 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct GittuorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
